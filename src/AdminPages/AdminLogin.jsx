acc49f98c028d00fff792fc19a730f9177df3c3d
Sudha Pantha <sudhapantha111@gmail.com>
2025-01-01 21:24:02 +0545

    changes on login

--- src/AdminPages/AdminLogin.jsx
+++ src/AdminPages/AdminLogin.jsx
@@ -40,12 +40,16 @@ function AdminLogin() {
         <NavBar />
         <form
           onSubmit={loginAdmin}
-          className="bg-blue-300/20 p-10 max-w-md w-full mx-auto rounded-xl mt-30 shadow-lg backdrop-blur-md"
+          className="bg-white p-10 max-w-md w-full mx-auto rounded-xl mt-30 shadow-lg backdrop-blur-md"
         >
-          <p className="text-2xl mb-4 text-center">Login Page</p>
+          <p className="text-2xl mb-4 text-center">
+            <a href="/" className=" hover:text-blue-800">
+              Login Page
+            </a>
+          </p>
           <div>
             <input
-              className="mb-5 p-2 rounded-md w-full"
+              className="mb-5 p-2 rounded-md w-full border border-blue-300 focus:ring-2 focus:ring-light-blue-500 focus:border-light-blue-500"
               type="email"
               placeholder="Enter your mail here"
               onChange={(e) => setEmail(e.target.value)}
@@ -54,7 +58,7 @@ function AdminLogin() {
           </div>
           <div>
             <input
-              className="mb-5 p-2 rounded-md w-full"
+              className="mb-5 p-2 rounded-md w-full border border-blue-300 focus:ring-2 focus:ring-light-blue-500 focus:border-light-blue-500"
               type="password"
               placeholder="Enter Your Password here"
               onChange={(e) => setPassword(e.target.value)}
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -40,12 +40,16 @@ function Login() {
         <NavBar />
         <form
           onSubmit={loginStudent}
-          className="bg-blue-300/20 p-10 max-w-md w-full mx-auto rounded-xl mt-30 shadow-lg backdrop-blur-md"
+          className="bg-white p-10 max-w-md w-full mx-auto rounded-xl mt-30 shadow-lg"
         >
-          <p className="text-2xl mb-4 text-center">Login Page</p>
+          <p className="text-2xl mb-4 text-center">
+            <a href="/" className=" hover:text-blue-800">
+              Login Page
+            </a>
+          </p>
           <div>
             <input
-              className="mb-5 p-2 rounded-md w-full"
+              className="mb-5 p-2 rounded-md w-full border border-gray-300 focus:ring-2 focus:ring-light-blue-500 focus:border-light-blue-500"
               type="email"
               placeholder="Enter your mail here"
               onChange={(e) => setEmail(e.target.value)}
@@ -54,7 +58,7 @@ function Login() {
           </div>
           <div>
             <input
-              className="mb-5 p-2 rounded-md w-full"
+              className="mb-5 p-2 rounded-md w-full border border-blue-300 focus:ring-2 focus:ring-light-blue-500 focus:border-light-blue-500"
               type="password"
               placeholder="Enter Your Password here"
               onChange={(e) => setPassword(e.target.value)}
@@ -65,6 +69,21 @@ function Login() {
             <button className="bg-blue-500 mb-5 p-2 rounded-md w-full text-white text-xl hover:bg-blue-600 transition-all">
               Login
             </button>
+          </div>
+          <div className="flex text-center gap-32">
+            <div className="text-center">
+              <a
+                href="/forgot-password"
+                className="hover:text-blue-800 text-md"
+              >
+                Forgot Password?
+              </a>
+            </div>
+            <div className="text-center">
+              <a href="/admin-login" className="hover:text-blue-800 text-md">
+                Admin Login
+              </a>
+            </div>
           </div>
         </form>
       </div>
